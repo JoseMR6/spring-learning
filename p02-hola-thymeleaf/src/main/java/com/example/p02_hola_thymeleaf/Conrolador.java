7a695ae51332f0aceafa902b3cc93c27bec1611e
josemr <jmelguizoruano@gmail.com>
2024-10-12 18:58:07 +0200

    Listas

--- p02-hola-thymeleaf/src/main/java/com/example/p02_hola_thymeleaf/Conrolador.java
+++ p02-hola-thymeleaf/src/main/java/com/example/p02_hola_thymeleaf/Conrolador.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+//import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.example.domain.Individuo;
 
@@ -26,12 +29,27 @@ public class Conrolador {
         individuo.setEdad("24 años");
         individuo.setTelefono("12344556765");
 
+        Individuo individuo2 = new Individuo();
+        individuo2.setNombre("Antonio");
+        individuo2.setApellido("Perez");
+        individuo2.setCorreo("antonio@correo.com");
+        individuo2.setEdad("35 años");
+        individuo2.setTelefono("454735234");
+
+        //List<Individuo> individuos = new ArrayList();
+        //individuos.add(individuo);
+        //individuos.add(individuo2);
+
+        List<Individuo> individuos = Arrays.asList(individuo,individuo2);
+
         log.info("Ejecutando Controlador MVC");
         model.addAttribute("hola", hola);
         model.addAttribute("hola2", hola2);
         model.addAttribute("individuo", individuo);
+        model.addAttribute("individuos", individuos);
         //return "indice1";
-        return "indice2";
+        //return "indice2";
+        return "indice3";
     }
     
 }
